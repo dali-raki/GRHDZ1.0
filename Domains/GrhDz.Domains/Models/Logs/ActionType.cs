0f06589958402a4ffe1ac992acb68ca0b3425956
Dali Raki <Dali Raki@DESKTOP-5NBJR0Q>
2025-11-18 19:08:36 +0000

    'add logs'

---
+++ Domains/GrhDz.Domains/Models/Logs/ActionType.cs
@@ -0,0 +1,10 @@
+﻿namespace GrhDz.Domains.Models.Logs;
+
+public enum ActionType
+{
+    Insert = 1,
+    Update = 2,
+    Delete = 3,
+    Login = 4,
+    Logout = 5,
+}
---
+++ Domains/GrhDz.Domains/Models/Logs/LogAction.cs
@@ -0,0 +1,10 @@
+﻿namespace GrhDz.Domains.Models.Logs;
+
+public class LogAction
+{
+    public int Id { get; set; }
+    public ActionType ActionType { get; set; }
+    public string PerformedBy { get; set; }
+    public string Description { get; set; }
+    public DateTime ActionDate { get; set; }
+}
